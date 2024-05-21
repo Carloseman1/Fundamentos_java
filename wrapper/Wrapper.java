5f4440224442e10f5f5149f36c96aa7380f66496
Carlos Emanuel <emanuecarlos030@gmail.com>
2024-05-21 20:38:08 -0300

    new repository

---
+++ wrapper/Wrapper.java
@@ -0,0 +1,19 @@
+package wrapper;
+
+public class Wrapper {
+    
+    
+    public static void main(String[] args) {
+        int tipoInt = 1;
+        float tipoFloat = 1;
+        boolean tipoBoolean =  false;
+
+        //int - char 
+
+        Float tipoFloat2 = 1F;
+        Boolean tipoBoolean2 = false;
+        Integer tipoInteger = null;
+
+    }
+
+}
